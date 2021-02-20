382d33eee1e9f13753cb5206de51f676554f1ae7
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-20 12:41:52 +0300

    get passenger dat place holder

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-export default function Home() {
+export const getStaticProps = async () => {
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+      props: { passengers: data }
+  }
+
+}
+
+const Home = ({ passengers }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,9 +27,19 @@ export default function Home() {
         </h1>
         <hr/>
 
+        {passengers.map(passenger => (
+          <Link href={ '/passenger/' + passenger.id } key={ passenger.id }>
+            <a className={ styles.single }>
+              <h3>{ passenger.name }</h3>
+            </a>
+          </Link>
+        ))}
+
 
       </main>
 
     </div>
-  )
+  );
 }
+
+export default Home;
