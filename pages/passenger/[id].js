6bd9c47e938238f05fa4ba4bd2e187b07b642960
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-20 17:30:26 +0300

    CSS upts

--- pages/passenger/[id].js
+++ pages/passenger/[id].js
@@ -49,7 +49,6 @@ const Details = ({ passenger }) => {
                 <h1 className={styles.title}>Passenger Details:</h1>
                 <hr/>
                 <div className={styles.passengerDatas}>
-                    <br/><br/><br/>
                     <div className={styles.inlineItem}>
                         <h4> {passenger.name} </h4>{" "}
                         <EditModal currentName={passenger.name} buttonLabel={editIcon} />
@@ -59,20 +58,16 @@ const Details = ({ passenger }) => {
                     <p> {passenger.website} </p>
                     <p> {passenger.address.city} </p>
                     <ErrorModal buttonLabel="Error" />
+                    <Link href={'/'}>
+                        <a className={styles.goback}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/></svg>
+                            <span>Back to Home</span> 
+                        </a>
+                    </Link>
                 </div>
-                
             </main>
-            <footer>
-                <Link href={'/'}>
-                    <a className={styles.goback}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/></svg>
-                        <span>Back</span> 
-                    </a>
-                </Link>
-            </footer>
-        </div>
 
-        
+        </div>
      );
 }
  
