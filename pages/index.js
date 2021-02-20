744f38bb4f951874d430ed011f95147c5fcb2a53
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-21 01:37:40 +0300

    axios get data from api

--- pages/index.js
+++ pages/index.js
@@ -2,49 +2,32 @@ import React, { useState, useEffect,  Component } from 'react';
 import Link from 'next/link'
 import Head from 'next/head'
 import axios from 'axios';
-
 import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'reactstrap';
 
-
-const api = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/users',
-  headers: {
-    'X-auth-key': "token123"
-  }
-})
-
-export const getStaticProps = async () => {
-  const data = await api.get('/', {
-    params: {
-      _limit: 5,
-      _start: 0
-    }
-  }).then(({ data }) => data);
-
-  return {
-      props: { pasData: data }
-  }
-}
-
-const Home = ({ pasData }) => {
+const Home = () => {
   const [passengers, setPassengers] = useState([]);
+  const [pageId, setPageId] = useState(1);
 
   useEffect(() => {
-    getUsers();
+    getStaticProps();
     return () => {
       console.log("cleanup useeffect")
     }
   }, []);
 
-  const getUsers = async () => {
-    try {
-      let data = pasData;
-      setPassengers(data);
-    } catch (error) {
-      console.log(error);
-    }    
+  const getStaticProps = async () => {
+    const res = await axios.get('https://api.instantwebtools.net/v1/passenger?page=0&size=10');
+    setPassengers(res.data.data);
+  }
+
+  const getUserWithPage = async () =>{
+    const res = await axios.get(`https://api.instantwebtools.net/v1/passenger?page=${pageId}&size=10`);
+    setPassengers(passengers =>[
+      ...passengers,...res.data.data
+    ]);
+    setPageId(pageId+1)
   }
 
   return (
@@ -56,15 +39,15 @@ const Home = ({ pasData }) => {
       <main className={styles.main}>
         <h1 className={styles.title}>Our Passengers</h1><hr/>
 
-        {passengers.map(passenger => (
-          <Link href={ '/passenger/' + passenger.id } key={ passenger.id }>
+        {passengers.map((passobj) => (
+          <Link href={ '/passenger/' + passobj._id } key={ passobj._id }>
             <a className={ styles.single }>
-              <h3>{ passenger.name }</h3>
+              <h3 key={passobj._id}>{ passobj.name }</h3>
             </a>
           </Link>
         ))} 
-        <Button color="primary" onClick={() => setLimit(limit+2)}>Show More</Button>
 
+        <Button color="primary" onClick={ () => getUserWithPage() }>Show More</Button>
       </main>
     </div>
   );
